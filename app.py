8f4b970997b9b9356e23360d0f52c03545b70b11
Gpatel23 <gpatel582@gmail.com>
2024-03-06 21:17:37 -0500

    added image url to sql table, got image to appear on player page, simple table with stats on player page as well

--- app.py
+++ app.py
@@ -18,13 +18,33 @@ def search():
 
 @app.route('/playerInfo/<name>')
 def playerInfo(name):
-   conn = query.connect_to_database()
-   sql_query = f"""SELECT * FROM stats_23_24
-                  WHERE player = '{name}' """
+    tables = ['stats_23_24', 'stats_22_23', 'stats_21_22', 
+             'stats_20_21', 'stats_19_20', 'stats_18_19', 
+             'stats_17_18', 'stats_16_17', 'stats_15_16', 'stats_14_15']
+    all_results = {}
+    conn = query.connect_to_database()
+    for table in tables:
+        sql_query = f"""SELECT Pos, Tm, G, MP, FG, FGA,
+                        `FG%`, 3P, 3PA, `3P%`, FT, FTA, `FT%`, 
+                        TRB, AST, STL, BLK, TOV, PTS FROM {table}
+                        WHERE player = '{name}' AND Tm != 'Tot' """
                   
-   results = query.execute_query(conn, sql_query)
+        results = query.execute_query(conn, sql_query)
+        all_results[table] = results
 
-   return render_template('playerInfo.html', results = results)
+    image_sql_query = f"""SELECT image_url FROM stats_23_24 
+                        WHERE player = '{name}' """
+    image_url = query.execute_query(conn, image_sql_query)
+    # need to add date, opp team, result, ORB, DRB, PF, GMS(maybe), +/- 
+        #I think these should give more info, may even take some info out of the past 10 years stats
+    # last_games_sql_query = f"""SELECT Pos, Tm, G, MP, FG, FGA,
+    #                             `FG%`, 3P, 3PA, `3P%`, FT, FTA, `FT%`, 
+    #                             TRB, AST, STL, BLK, TOV, PTS FROM 'last_5_games' 
+    #                             WHERE player = '{name}' """
+    
+    # five_games = query.execute_query(conn, last_games_sql_query)
+    full_image_url = "http://cdn.ssref.net/scripts/image_resize.cgi?min=200&url=" + image_url[0][0]
+    return render_template('playerInfo.html', total_stats = all_results, image = full_image_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- webscrape/webscrape.py
+++ webscrape/webscrape.py
@@ -1,44 +1,105 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import modules.query as q
 from sqlalchemy import create_engine
 
+    
+username = 'eric'
+password = 'nomeat555'
+host = '96.38.123.26'  # or your host, e.g., '127.0.0.1'
+database = 'basketballstats'
+
+
+def getTable():
 # URL of the NBA players statistics page on Basketball-Reference
-url = 'https://www.basketball-reference.com/leagues/NBA_2024_per_game.html'
+    url = 'https://www.basketball-reference.com/leagues/NBA_2024_per_game.html'
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Find the table containing player statistics
+        table = soup.find('table', {'id': 'per_game_stats'})
+        
+        # Extract column headers
+        headers = [th.text.strip() for th in table.find('thead').find_all('th')]
+        
+        # Initialize an empty list to store player data
+        player_data = []
+        
+        # Iterate over rows in the table body
+        for row in table.find('tbody').find_all('tr'):
+            # Extract data from each cell in the row
+            player_row = [td.text.strip() for td in row.find_all(['th', 'td'])]
+            # Add player data to the list
+            player_data.append(player_row)
+        
+        # Convert the list of player data into a DataFrame
+        df = pd.DataFrame(player_data, columns=headers)
+        
+
+        # Create a MySQL engine
+        engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}/{database}')
+
+        df.to_sql('stats_23-24', con=engine, if_exists='replace', index=False)
+
+# def scrape_image_url(player_url):
+#     response = requests.get(player_url)
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.content, 'html.parser')
+#         og_image_tag = soup.find("meta", property="og:image")
+#         if og_image_tag:
+#             return og_image_tag["content"]
+#     return None
+
+def add_image():
+    url = 'https://www.basketball-reference.com/leagues/NBA_2024_per_game.html'
 
 # Send a GET request to the URL
-response = requests.get(url)
-
+    response = requests.get(url)
+    #print(response.status_code)
 # Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Find the table containing player statistics
-    table = soup.find('table', {'id': 'per_game_stats'})
-    
-    # Extract column headers
-    headers = [th.text.strip() for th in table.find('thead').find_all('th')]
-    
-    # Initialize an empty list to store player data
-    player_data = []
-    
-    # Iterate over rows in the table body
-    for row in table.find('tbody').find_all('tr'):
-        # Extract data from each cell in the row
-        player_row = [td.text.strip() for td in row.find_all(['th', 'td'])]
-        # Add player data to the list
-        player_data.append(player_row)
-    
-    # Convert the list of player data into a DataFrame
-    df = pd.DataFrame(player_data, columns=headers)
-    
-    username = 'eric'
-    password = 'nomeat555'
-    host = '96.38.123.26'  # or your host, e.g., '127.0.0.1'
-    database = 'basketballstats'
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Find the table containing player statistics
+        table = soup.find('table', {'id': 'per_game_stats'})
+        
+        # Iterate through each row of the table
+        rows = table.find_all('tr')[1:]  # Skip the header row
+        for row in rows:
+            # Find player name
+            player_name_elem = row.find('td', {'data-stat': 'player'})
+            if player_name_elem:
+                player_name = player_name_elem.text
+                player_link = row.find('td', {'data-stat': 'player'}).find('a')['href']
+                player_id = player_link.split("/")[-1].split(".")[0]
+                image_url = 'https://www.basketball-reference.com/req/202106291/images/headshots/' + player_id + '.jpg'
+            else:
+                player_name = None
+                image_url = None
+            print(player_name)
+            
+            conn = q.connect_to_database()
+            sql_query = f"""UPDATE basketballstats.stats_23_24 
+                SET image_url = '{image_url}'
+                WHERE player = "{player_name}" AND image_url IS NULL """
+            
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            #print(image_url)
 
-    # Create a MySQL engine
-    engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}/{database}')
+    else:
+        print("failure")
 
-    df.to_sql('stats_23-24', con=engine, if_exists='replace', index=False)
+
+
