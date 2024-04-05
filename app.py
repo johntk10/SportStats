0cca6f45c4b56e2c78fbcd8389665511bd981e39
Gpatel23 <gpatel582@gmail.com>
2024-04-04 20:16:45 -0400

    added totals at bottom of all tables, added tables to gameInfo.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import modules.query as query
 
 app = Flask(__name__)
 
-
 def process_data(raw_data):
     processed_data = []
     for game in raw_data:
@@ -32,7 +31,6 @@ def process_data(raw_data):
         })
     return processed_data
 
-# Define your routes
 @app.route('/')
 def home():
     conn = query.connect_to_database()
@@ -52,23 +50,18 @@ def home():
         if matching_result:
             new_game = result + matching_result[3:]
             merged_results.append(new_game)
-            
-            # results.remove(result)
             results.remove(matching_result)
-    # print(merged_results)
 
     new_results = process_data(merged_results)
     return render_template('home.html', results = new_results, count = len(new_results))
 
 
-
 @app.route('/search', methods=['POST'])
 def search():
     query_string = request.json.get('query', '')
-    # Call the Python function to execute the query
     results = query.name_search(query_string)
-    # Return the results as JSON
     return jsonify(results=results)
+
 
 @app.route('/playerInfo/<name>')
 def playerInfo(name):
@@ -89,18 +82,46 @@ def playerInfo(name):
     image_sql_query = f"""SELECT image_url FROM stats_23_24 
                         WHERE player = '{name}' """
     image_url = query.execute_query(conn, image_sql_query)
-    # need to add date, opp team, result, ORB, DRB, PF, GMS(maybe), +/- 
-        #I think these should give more info, may even take some info out of the past 10 years stats
+    
     last_games_sql_query = f"""SELECT Date, Team, `@`, OPP, Result, MP, FG, FGA,
                                 `FG%`, 3P, 3PA, `3P%`, FT, FTA, `FT%`, 
                                 ORB, DRB, TRB, AST, STL, BLK, TOV, PF, PTS, GmSc, `+/-` FROM `last_5_games` 
                                 WHERE player = "{name}" """
     
     five_games = query.execute_query(conn, last_games_sql_query)
-    # print(five_games)
     full_image_url = "http://cdn.ssref.net/scripts/image_resize.cgi?min=200&url=" + image_url[0][0]
 
-    return render_template('playerInfo.html', total_stats = all_results, image = full_image_url, name = name, five_games = five_games)
+    season_sums = [0] * 17
+    last5_sums = [0] * 21
+    k = 1
+    games = 0
+
+    for key, value in all_results.items():
+        for item in value:
+            games += int(item[2])
+            for val in item[3:]:
+                season_sums[k] += float(float(val) * int(item[2]) or 0) 
+                k+=1
+            k = 1
+
+    for i in range(len(season_sums)):
+        season_sums[i] = "{:.3g}".format(season_sums[i] / games)
+
+    season_sums[0] = games
+    k = 0
+
+    for value in five_games:
+        for val in value[5:]:
+            last5_sums[k] += float(val or 0) 
+            k+=1
+        k = 0
+
+    for j in range(len(last5_sums)):
+        last5_sums[j] = round(last5_sums[j] / len(five_games), 3)
+    
+    return render_template('playerInfo.html', total_stats = all_results, image = full_image_url, name = name,
+                           five_games = five_games, season_sums = season_sums, last5_sums = last5_sums)
+
 
 @app.route('/gameInfo/<team>/vs/<opp>')
 def gameInfo(team, opp):
@@ -116,9 +137,33 @@ def gameInfo(team, opp):
         if(t[2] != team):
             team2_info.append(t)
         else: team1_info.append(t)
-    print(team1_info)
-    print(team2_info)
-    return render_template('gameInfo.html', team1_info = team1_info, team2 = team2_info, team = team, team2 = opp)
+
+    team1_sums = [0] * 21
+    team2_sums = [0] * 21
+
+    for row in team1_info:
+        for i in range(21):
+            if i == 3 or i == 6 or i == 9 or i == 19: team1_sums[i] += float(row[i + 7] or 0) 
+            else: team1_sums[i] += int(row[i + 7] or 0)  
+
+    # team1_sums[0] = 240
+    team1_sums[3] = round(team1_sums[1] / team1_sums[2], 3)
+    team1_sums[6] = round(team1_sums[4] / team1_sums[5], 3)
+    team1_sums[9] = round(team1_sums[7] / team1_sums[8], 3)
+
+    for row in team2_info:
+        for i in range(21):
+            if i == 3 or i == 6 or i == 9 or i == 19: team2_sums[i] += float(row[i + 7] or 0) 
+            else: team2_sums[i] += int(row[i + 7] or 0)  
+
+    # team2_sums[0] = 240
+    team2_sums[3] = round(team2_sums[1] / team2_sums[2], 3)
+    team2_sums[6] = round(team2_sums[4] / team2_sums[5], 3)
+    team2_sums[9] = round(team2_sums[7] / team2_sums[8], 3)
+
+    return render_template('gameInfo.html', team1_info = team1_info, team2_info = team2_info, team = team, 
+                           team2 = opp, team1_sums = team1_sums, team2_sums = team2_sums)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
